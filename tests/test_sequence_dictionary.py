86168794e9db9aaa116973f84713a7dd2d2dd4f7
TimeMatters <49761113+karellopez@users.noreply.github.com>
2025-09-12 11:18:19 +0200

    Revert "Handle SBRef and physio naming"

--- tests/test_sequence_dictionary.py
+++
@@ -1,6 +0,0 @@
-from bids_manager import dicom_inventory
-
-
-def test_sbref_patterns_cover_old_refscan():
-    for seq in ["type-ref", "reference", "refscan", "ref"]:
-        assert dicom_inventory.guess_modality(seq) == "SBRef"
